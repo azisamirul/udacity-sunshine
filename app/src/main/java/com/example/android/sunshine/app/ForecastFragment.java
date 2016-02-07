19fad151f1dc1be1e693389382713657e3dee81f
azisamirul <amirulbaharazis@gmail.com>
2016-02-07 17:27:17 +0700

    Add ForecastFragment.java

---
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -0,0 +1,119 @@
+package com.example.android.sunshine.app;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by azisamirul on 07/02/2016.
+ */
+public class ForecastFragment extends Fragment {
+    ArrayAdapter<String> mForecastAdapter;
+
+    public ForecastFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+
+        String[] forecastArray = {
+                "Today - Sunny - 88/63",
+                "Tomorrow - Foggy - 70/40",
+                "Weds - Cloudly - 72/63",
+                "Thurs - Asteroids - 75/65",
+                "Fri - Heavy Rain - 65/56",
+                "Sat - Help Trapped In Weatherstation - 60/51",
+                "Sun - Sunny - 60/68"
+        };
+        List<String> weekForecast = new ArrayList<String>(Arrays.asList(forecastArray));
+
+        mForecastAdapter = new ArrayAdapter<String>(
+                getActivity(),
+                R.layout.list_item_forecast,
+                R.id.list_item_forecast_textview,
+                weekForecast
+        );
+
+
+        ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+        listView.setAdapter(mForecastAdapter);
+        return rootView;
+    }
+
+}
+
+ class FetchWeatherTask extends AsyncTask<Void, Void, Void> {
+    private final String LOG_TAG = FetchWeatherTask.class.getSimpleName();
+
+    @Override
+    protected Void doInBackground(Void... params) {
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader = null;
+
+        String forecastJsonStr = null;
+
+        try {
+            String baseUrl = "http://api.openweathermap.org/data/2.5/forecast/daily?q=55654&mode=json&units=metric&cnt=7";
+            String apiKey = "&APPID=" + BuildConfig.OPEN_WEATHER_MAP_API_KEY;
+            URL url = new URL(baseUrl.concat(apiKey));
+
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+
+            InputStream inputStream = urlConnection.getInputStream();
+            StringBuffer buffer = new StringBuffer();
+            if (inputStream == null) {
+                return null;
+            }
+
+            reader = new BufferedReader(new InputStreamReader(inputStream));
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                buffer.append(line + "\n");
+            }
+            if (buffer.length() == 0) {
+                return null;
+            }
+            forecastJsonStr = buffer.toString();
+
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Error", e);
+
+            return null;
+        }finally{
+            if(urlConnection!=null){
+                urlConnection.disconnect();
+            }
+            if(reader!=null){
+                try{
+                    reader.close();
+                }catch(final IOException e){
+                    Log.e(LOG_TAG,"Error closing stream",e);
+                }
+            }
+        }
+        return null;
+    }
+}
